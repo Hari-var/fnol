81532c0c7c1a20119f434230d9bb84a7d1b3af49
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-09-26 07:40:54 +0530

    removed ../helpers/config

--- src/Components/ClaimDetails.jsx
+++ src/Components/ClaimDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaFileAlt, FaCalendarAlt, FaMapMarkerAlt, FaRupeeSign, FaExclamationTriangle, FaRobot, FaUser, FaImage, FaDownload, FaTimes, FaEdit, FaCheck, FaBan, FaSave } from "react-icons/fa";
-import { root_path } from "../helpers/config";
 import "../styles/claimdetails.css";
 
 export default function ClaimDetails() {
